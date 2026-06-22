ec9a79b13e461ea81c283ce31bef812fe6aac66b
teiii <79211121+somehowtei@users.noreply.github.com>
2026-06-22 14:07:25 +0300

    Add RTC module for timekeeping functionality

---
+++ RTC_module.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+#include "RTClib.h"
+RTC_DS3231 rtc;
+
+void setup() 
+{
+  Serial.begin(9600);
+  if (!rtc.begin()) 
+  {
+    Serial.println("Nu gasesc RTC!");
+    while (1);
+  }
+
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+}
+
+void loop() 
+{
+  DateTime now=rtc.now();
+  Serial.print(now.day());
+  Serial.print("/");
+  Serial.print(now.month());
+  Serial.print("/");
+  Serial.print(now.year());
+  Serial.print(" ");
+  Serial.print(now.hour());
+  Serial.print(":");
+  Serial.print(now.minute());
+  Serial.print(":");
+  Serial.println(now.second());
+
+  delay(200);
+}
